b4a2fdf089d0d41f5f99764840c4104c0169c513
nakanuma <k023g0121@g.neec.ac.jp>
2024-07-31 14:59:31 +0900

    SceneManagerクラス作成

---
+++ project/SceneManager.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "BaseScene.h"
+
+// シーン管理
+class SceneManager
+{
+public:
+	~SceneManager();
+
+	// 次シーン予約
+	void SetNextScene(BaseScene* nextScene) { nextScene_ = nextScene; }
+
+	// 更新
+	void Update();
+
+	// 描画
+	void Draw();
+
+private:
+	// 今のシーン（実行中シーン）
+	BaseScene* scene_ = nullptr;
+	// 次のシーン
+	BaseScene* nextScene_ = nullptr;
+};
+
