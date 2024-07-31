f51ac25bd48eba2dbf8f996b2f4bc996cf4cc48e
nakanuma <k023g0121@g.neec.ac.jp>
2024-07-31 12:53:09 +0900

    ブランチの作成

---
+++ project/TitleScene.cpp
@@ -0,0 +1,133 @@
+#include "TitleScene.h"
+#include "ImguiWrapper.h"
+#include "DirectXBase.h"
+#include "SRVManager.h"
+#include "SpriteCommon.h"
+
+void TitleScene::Initialize()
+{
+	DirectXBase* dxBase = DirectXBase::GetInstance();
+
+	// カメラのインスタンスを生成
+	camera = new Camera({ 0.0f, 0.0f, -10.0f }, { 0.0f, 0.0f, 0.0f }, 0.45f);
+	Camera::Set(camera); // 現在のカメラをセット
+
+	// SpriteCommonの生成と初期化
+	spriteCommon = new SpriteCommon();
+	spriteCommon->Initialize(DirectXBase::GetInstance());
+
+	// TextureManagerの初期化
+	TextureManager::Initialize(dxBase->GetDevice(), SRVManager::GetInstance());
+
+	// SoundManagerの初期化
+	soundManager = new SoundManager();
+	soundManager->Initialize();
+
+	///
+	///	↓ ゲームシーン用
+	///	
+
+	// Texture読み込み
+	uint32_t uvCheckerGH = TextureManager::Load("resources/Images/uvChecker.png", dxBase->GetDevice());
+
+	// スプライトの生成と初期化
+	sprite_ = new Sprite();
+	sprite_->Initialize(spriteCommon, uvCheckerGH);
+
+
+	// モデル読み込み
+	model_ = ModelManager::LoadObjFile("resources/Models", "sphere.obj", dxBase->GetDevice());
+
+	// 3Dオブジェクトの生成とモデル指定
+	object_ = new Object3D();
+	object_->model_ = &model_;
+	object_->transform_.rotate = { 0.0f, 3.14f, 0.0f };
+
+	// 音声読み込み
+	soundData_ = soundManager->LoadWave("resources/Sounds/yay.wav");
+}
+
+void TitleScene::Finalize()
+{
+	// 音声データ開放
+	soundManager->Unload(&soundData_);
+	// SoundManager開放
+	delete soundManager;
+
+	// 3Dオブジェクト開放
+	delete object_;
+
+	// Sprite開放
+	delete sprite_;
+
+	// SpriteCommon開放
+	delete spriteCommon;
+
+	// カメラの開放
+	delete camera;
+}
+
+void TitleScene::Update()
+{
+	// スプライトの更新
+	sprite_->Update();
+
+	// 3Dオブジェクトの更新
+	object_->UpdateMatrix();
+}
+
+void TitleScene::Draw()
+{
+	DirectXBase* dxBase = DirectXBase::GetInstance();
+	SRVManager* srvManager = SRVManager::GetInstance();
+
+	// 描画前処理
+	dxBase->PreDraw();
+	// 描画用のDescriptorHeapの設定
+	ID3D12DescriptorHeap* descriptorHeaps[] = { srvManager->descriptorHeap.heap_.Get() };
+	dxBase->GetCommandList()->SetDescriptorHeaps(1, descriptorHeaps);
+	// ImGuiのフレーム開始処理
+	ImguiWrapper::NewFrame();
+	// カメラの定数バッファを設定
+	Camera::TransferConstantBuffer();
+
+	///
+	///	↓ ここから3Dオブジェクトの描画コマンド
+	/// 
+
+	// 3Dオブジェクト描画
+	object_->Draw();
+
+	///
+	///	↑ ここまで3Dオブジェクトの描画コマンド
+	/// 
+
+	// Spriteの描画準備。全ての描画に共通のグラフィックスコマンドを積む
+	spriteCommon->PreDraw();
+
+	///
+	/// ↓ ここからスプライトの描画コマンド
+	/// 
+
+	// スプライトの描画
+	sprite_->Draw();
+
+	///
+	/// ↑ ここまでスプライトの描画コマンド
+	/// 
+
+	ImGui::Begin("window");
+
+	if (ImGui::Button("PlaySound")) {
+		soundManager->PlayWave(soundData_);
+	}
+
+	ImGui::End();
+
+	// ImGuiの内部コマンドを生成する
+	ImguiWrapper::Render(dxBase->GetCommandList());
+	// 描画後処理
+	dxBase->PostDraw();
+	// フレーム終了処理
+	dxBase->EndFrame();
+}
---
+++ project/TitleScene.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "Camera.h"
+#include "SpriteCommon.h"
+#include "TextureManager.h"
+#include "Sprite.h"
+#include "ModelManager.h"
+#include "Object3D.h"
+#include "SoundManager.h"
+
+// ゲームプレイシーン
+class TitleScene
+{
+public:
+	// 初期化
+	void Initialize();
+
+	// 終了
+	void Finalize();
+
+	// 毎フレーム更新
+	void Update();
+
+	// 描画
+	void Draw();
+
+private:
+	Camera* camera = nullptr;
+	SpriteCommon* spriteCommon = nullptr;
+	SoundManager* soundManager = nullptr;
+
+	///
+	/// ↓ ゲームシーン用
+	///
+
+	// スプライト
+	Sprite* sprite_;
+
+	// モデルデータ
+	ModelManager::ModelData model_;
+	// 3Dオブジェクト
+	Object3D* object_;
+
+	// 音声データ
+	SoundManager::SoundData soundData_;
+};
+
