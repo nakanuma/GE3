d95c029c0d0dc18286f4d7bd8d81ad40b3aedbb8
nakanuma <k023g0121@g.neec.ac.jp>
2024-08-04 12:00:03 +0900

    ブランチの作成

--- project/MyGame.cpp
+++ project/MyGame.cpp
@@ -23,7 +23,7 @@ void MyGame::Initialize()
 	sceneFactory_ = new SceneFactory();
 	SceneManager::GetInstance()->SetSceneFactory(sceneFactory_);
 	// シーンマネージャに最初のシーンをセット
-	SceneManager::GetInstance()->ChangeScene("TITLE");
+	SceneManager::GetInstance()->ChangeScene("GAMEPLAY");
 }
 
 void MyGame::Finalize()
